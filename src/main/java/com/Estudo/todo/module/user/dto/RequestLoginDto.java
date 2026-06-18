4949a23e012631151e50c7cc69e6cbf3bdb374dd
Fabio Farias Coutinho <fabiofariascoutinho@MacBook-Air-de-Fabio.local>
2026-06-18 20:09:12 -0300

    adjustments in use Dtos

--- src/main/java/com/Estudo/todo/module/user/dto/RequestLoginDto.java
+++ src/main/java/com/Estudo/todo/module/user/dto/RequestLoginDto.java
@@ -1,14 +1,11 @@
 package com.Estudo.todo.module.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record RequestLoginDto(
         @NotBlank(message = "Nome de usuario e Obrigatorio")
         String userName,
         @NotBlank(message = "Senha e Obrigatória")
-        @Size(min = 6, max = 50)
         String password
 ) {
 }
--- src/main/java/com/Estudo/todo/module/user/dto/RequestRegisterDto.java
+++ src/main/java/com/Estudo/todo/module/user/dto/RequestRegisterDto.java
@@ -1,12 +1,14 @@
 package com.Estudo.todo.module.user.dto;
 
-import com.Estudo.todo.module.user.userRole.UserRole;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RequestRegisterDto(
         @NotBlank(message = "Nome de usuario e Obrigatorio")
+        @Size(min = 3, max = 255)
         String userName,
         @NotBlank(message = "Senha e Obrigatória")
+        @Size(min = 6, max = 50)
         String password
-){
+) {
 }
